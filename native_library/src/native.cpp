6edd85ebeca8f3a30ff5ab48289b1e3efb68dbc0
nicozink <nico@nfzink.com>
2019-09-01 19:28:46 +0100

    Initial checkin

---
+++ native_library/src/native.cpp
@@ -0,0 +1,6 @@
+#include "native.h"
+
+int Add(int a, int b)
+{
+	return a - b;
+}
---
+++ net_proj/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+class User32
+{
+    //[DllImport("user32.dll")]
+    //public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+
+    //[DllImport("libc", EntryPoint = "read")]
+    //public static extern int Read(int handle, byte[] buf, int n);
+}
+
+class Math
+{
+    [DllImport("native_library")]
+    public static extern int Add(int a, int b);
+}
+
+namespace InteropTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("CWD: {0}", System.IO.Directory.GetCurrentDirectory());
+
+            //var buffer = new byte[100];
+            //User32.Read(0, buffer, 100);
+
+            //Console.WriteLine("Input value is:" + Encoding.UTF8.GetString(buffer));
+
+            //User32.MessageBox(IntPtr.Zero, "Command-line message box", "Attention!", 0);
+
+            int result = Math.Add(1, 2);
+            Console.WriteLine("Hello World! {0}", result);
+        }
+    }
+}
